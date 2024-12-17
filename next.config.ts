e63652c9ee278b14c3ca7a9f8fed07131297b56f
Rohan Yashraj Gupta <rohanyashraj@gmail.com>
2024-12-17 23:05:46 +0530

    package update

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/rohanyashrajportfolioyt",
-  output: "export", // <=== enables static exports
-  reactStrictMode: true,
 };
 
 export default nextConfig;
